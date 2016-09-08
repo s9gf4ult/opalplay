6189e3fd8490178ce999a8c45e6a16cd9501ca8a
Alexey Uimanov <s9gf4ult@gmail.com>
2016-09-08 11:49:06 +0300

    create simple scheme

---
+++ scheme.sql
@@ -0,0 +1,31 @@
+CREATE EXTENSION "uuid-ossp" ;
+
+CREATE TABLE orders
+  ( id uuid primary key DEFAULT uuid_generate_v4()
+  , "name" text
+  , created_at timestamptz not null default now()
+  ) ;
+
+CREATE TABLE products
+  ( id uuid primary key DEFAULT uuid_generate_v4()
+  , "name" text
+  , order_id uuid not null references orders(id)
+  , product_type text not null
+  , product_id uuid not null
+  , created_at timestamptz not null default now()
+  ) ;
+
+CREATE TABLE airticket_product
+  ( id uuid primary key DEFAULT uuid_generate_v4()
+  , created_at timestamptz not null default now()
+  ) ;
+
+CREATE TABLE surchage_product
+  ( id uuid primary key DEFAULT uuid_generate_v4()
+  , created_at timestamptz not null default now()
+  ) ;
+
+CREATE TABLE refund_product
+  ( id uuid primary key DEFAULT uuid_generate_v4()
+  , created_at timestamptz not null default now()
+  ) ;
